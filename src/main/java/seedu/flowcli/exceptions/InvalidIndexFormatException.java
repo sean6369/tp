bf9ca8fa18561fb33ecc888f1e4e54fc979651c6
Sean <seanleesukiat@gmail.com>
2025-10-27 00:04:31 +0800

    Update InvalidIndexFormatException error output

--- src/main/java/seedu/flowcli/exceptions/InvalidIndexFormatException.java
+++ src/main/java/seedu/flowcli/exceptions/InvalidIndexFormatException.java
@@ -3,6 +3,6 @@ package seedu.flowcli.exceptions;
 public class InvalidIndexFormatException extends FlowCLIException {
     public InvalidIndexFormatException(String index, String indexType) {
         super("Invalid " + indexType + " index: " + index + 
-              ". Use the numeric index shown in the list.");
+              ". Use the numeric " + indexType + " index shown in 'list --all'.");
     }
 }
